d66b93a1339764642c1876c53cf7fad7526fa4e2
Dan Parry <parryd424@gmail.com>
2017-06-06 17:47:24 -0400

    Done.

--- components/PetBrowser.js
+++ components/PetBrowser.js
@@ -6,7 +6,16 @@ class PetBrowser extends React.Component {
   render() {
     return (
       <div className="ui cards">
-        <code>&lt;Pet /&gt;</code> &nbsp; components should go here
+        // <code>&lt;Pet /&gt;</code> &nbsp; components should go here
+        {this.props.pets.map( (singlePet)=>{
+
+          if (this.props.adoptedPets.includes(singlePet)){
+            return  <Pet pet={singlePet} isAdopted="true" />
+          } else {
+            return  <Pet pet={singlePet} isAdopted="false" onAdoptPet={this.props.onAdoptPet} />
+          }
+
+        } )}
       </div>
     );
   }
